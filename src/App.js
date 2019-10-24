9e89911356761a24b5e7e660c59ca41254a04a89
kewlnes2000 <kewlnes2000@gmail.com>
2019-10-25 00:25:15 +0800

    add postlist  Create Edit & Login test

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from "react";
 import jsonServerProvider from "ra-data-json-server";
-import { Admin, Resource, EditGuesser } from "react-admin";
+import { Admin, Resource } from "react-admin";
+//驗證登入狀態
+import AuthProvider from './AuthProvider';
+//Component
+import Dashboard from './Component/Dashboard';
 import { UserList } from "./Component/UserList/";
-import { PostList } from "./Component/PostList/";
+import { PostList, PostEdit, PostCreate } from "./Component/PostList/";
+//icon
+import PostIcon from '@material-ui/icons/Book';
+import UserIcon from '@material-ui/icons/Group';
 
 const dataProvider = jsonServerProvider("http://jsonplaceholder.typicode.com");
 const App = () => (
-  <Admin dataProvider={dataProvider}>
-    <Resource name="posts" list={PostList} edit={EditGuesser} />
-    <Resource name="users" list={UserList} />
+  <Admin authProvider={AuthProvider} dashboard={Dashboard} dataProvider={dataProvider}>
+    <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
+    <Resource name="users" list={UserList} icon={UserIcon} />
   </Admin>
 );
 
--- src/Component/PostList/index.js
+++ src/Component/PostList/index.js
@@ -1,25 +1,68 @@
 import React from "react";
 import {
-  List,
-  Datagrid,
-  DisabledInput,
-  TextField,
-  ReferenceField,
-  LongTextInput,
-  SelectInput,
-  EditButton,
+    List,
+    Datagrid,
+    TextField,
+    ReferenceField,
+    EditButton,
+    Edit,
+    SimpleForm,
+    DisabledInput,
+    ReferenceInput,
+    SelectInput,
+    TextInput,
+    LongTextInput,
+    Create,
+    Filter
 } from "react-admin";
 
+const PostTitle = ({ record }) => {
+    return <span>{record ? `${record.title}` : ''}</span>;
+};
+
+const PostFilter = (props) => (
+    <Filter {...props}>
+        <TextInput label="Search" source="q" alwaysOn />
+        <ReferenceInput label="User" source="userId" reference="users" allowEmpty>
+            <SelectInput optionText="name" />
+        </ReferenceInput>
+    </Filter>
+);
+
 export const PostList = props => (
-  <List {...props}>
-    <Datagrid>
-      <DisabledInput source="id" />
-      <ReferenceField source="userId" reference="users">
-        <SelectInput optionText="name" />
-      </ReferenceField>
-      <TextField source="title" />
-      <LongTextInput source="body" />
-      <EditButton />
-    </Datagrid>
-  </List>
+    <List filters={<PostFilter />} {...props}>
+        <Datagrid>
+            <TextField source="id" />
+            <ReferenceField source="userId" reference="users">
+                <TextField source="name" />
+            </ReferenceField>
+            <TextField source="title" />
+            <EditButton />
+        </Datagrid>
+    </List>
+);
+
+export const PostEdit = props => (
+    <Edit title={<PostTitle />} {...props}>
+        <SimpleForm>
+            <DisabledInput source="id" />
+            <ReferenceInput source="userId" reference="users">
+                <SelectInput optionText="name" />
+            </ReferenceInput>
+            <TextInput source="title" />
+            <LongTextInput source="body" />
+        </SimpleForm>
+    </Edit>
+);
+
+export const PostCreate = props => (
+    <Create {...props}>
+        <SimpleForm>
+            <ReferenceInput source="userId" reference="users">
+                <SelectInput optionText="name" />
+            </ReferenceInput>
+            <TextInput source="title" />
+            <LongTextInput source="body" />
+        </SimpleForm>
+    </Create>
 );
